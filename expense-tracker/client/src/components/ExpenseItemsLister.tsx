678e6f0992f8ac43ac3ecabab6376ddee6cdaf2b
afroz <afroz3002@gmail.com>
2023-11-25 16:03:12 +0530

    Add files via upload

---
+++ expense-tracker/client/src/components/ExpenseItemsLister.tsx
@@ -0,0 +1,80 @@
+
+import { Table } from "react-bootstrap"
+import IExpenseItem from "../models/expense"
+
+type ExpenseItemsListerModel = {
+  expenseItems: IExpenseItem[];
+}
+
+const ExpenseItemsLister = ({ expenseItems }: ExpenseItemsListerModel) => {
+
+  const testExpenseItems: IExpenseItem[] = [
+    {
+      "expenseDescription": "Internet Expenses",
+      "payeeName": "Ramesh",
+      "price": 1100,
+      "date": new Date(),
+      "id": 1
+    },
+    {
+      "expenseDescription": "Bike Maintainence Expenses",
+      "payeeName": "Rahul",
+      "price": 3500,
+      "date": new Date(),
+      "id": 2
+    },
+  ]
+
+  const getExpenseItems = () => {
+    return expenseItems;
+  }
+
+  const formatDate = (dateObjFromServer: Date) => {
+    const dateObj = new Date(dateObjFromServer);
+    return dateObj.getDate() + "-" + (dateObj.getMonth() + 1) + "-"
+      + dateObj.getFullYear();
+  }
+
+  return (
+    <div>
+
+      <h3>Expenses View</h3>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Expense Description</th>
+            <th>Payee Name</th>
+            <th>Expense Date</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+
+          {
+            getExpenseItems().map((expenseItem, index) => {
+
+              return (
+
+                <tr>
+                  <td>{(index + 1)}</td>
+                  <td>{expenseItem.expenseDescription}</td>
+                  <td>{expenseItem.payeeName}</td>
+                  <td>{formatDate(expenseItem.date)}</td>
+                  <td>{expenseItem.price}</td>
+                </tr>
+
+              )
+            })
+          }
+
+
+        </tbody>
+      </Table>
+
+    </div>
+  )
+
+}
+
+export { ExpenseItemsLister }
